bbe443c918808f70dd6c9c70313503b1414559ac
sorryz23 <sorryz23@gmail.com>
2022-12-21 21:40:27 -0500

    fixed eslint added prettier

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const path = require('path')
+const express = require('express')
+const session = require('express-session')
+const exphbs = require('express-handlebars')
+const routes = require('./controllers')
+const helpers = require('./utils/helpers')
 
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const app = express()
+const PORT = process.env.PORT || 3001
