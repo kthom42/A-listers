44cdb4df6d51036a0d52c257d7beb4b54d0d5e53
kthom42 <kmgt42@gmail.com>
2022-12-22 21:35:29 -0500

    modified schema file

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO items (id, product_name, catagory_name, trade_for)
+VALUES (0001, "67 pontiac firebird", "automotive", "64 ford mustang"),
+       (0002, "2015 dodge challeger","automotive", "2013 or newer chevy camero"),
+       (0003, "playstation 5", "electronics", "xbox series x"),
+       (0004, "2012 ford f150", "automotive", "harley 1995 or newer"),
+       (0005, "lazyboy", "free", "must picke up or pay for shipping"),
+       (0006, "1902 silver dollar", "collectibles", "inquire about trade"),
+       (0007, "holographic charizard", "collectibles", "blastoise")
