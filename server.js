485bd5f3933a6686dc20a74360ef37dd8f80a44d
Matthew <MatthewGebara@gmail.com>
2023-01-04 19:54:33 -0500

    Server code

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-
+const hbs = exphbs.create({});
 const app = express()
 const PORT = process.env.PORT || 3001
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    res.render('homepage')
+  })
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+  })
